9254b05655a4db1c2995ab856e40be498dbd67c3
Andrea Casalino <andrecasa91@gmail.com>
2021-12-25 21:20:58 +0100

    consuming training tools

--- Lib/Header/train/Trainable.h
+++ Lib/Header/train/Trainable.h
@@ -9,67 +9,80 @@
 #define EFG_TRAIN_TRAINABLE_H
 
 #include <categoric/Group.h>
-#include <train/TrainSet.h>
-#include <structure/components/NodesAware.h>
-#include <structure/components/BeliefAware.h>
-#include <structure/components/ThreadPoolAware.h>
-#include <structure/components/StructureTunableAware.h>
 #include <list>
+#include <structure/components/BeliefAware.h>
+#include <structure/components/NodesAware.h>
+#include <structure/components/StructureTunableAware.h>
+#include <structure/components/ThreadPoolAware.h>
+#include <train/TrainSet.h>
+
+#include <TrainingTools/Trainer.h>
 
 namespace EFG::train {
-    class TrainHandler {
-    public:
-        // recompute alfa part and return it when call getGradientAlpha
-        virtual void setTrainSet(TrainSetPtr trainSet, const std::set<categoric::VariablePtr>& modelVariables) = 0;
+class TrainHandler {
+public:
+  // recompute alfa part and return it when call getGradientAlpha
+  virtual void
+  setTrainSet(TrainSetPtr trainSet,
+              const std::set<categoric::VariablePtr> &modelVariables) = 0;
 
-        virtual float getGradientAlpha() = 0;
-        virtual float getGradientBeta() = 0;
+  virtual float getGradientAlpha() = 0;
+  virtual float getGradientBeta() = 0;
 
-        virtual void setWeight(const float& w) = 0;
-    };
-    typedef std::unique_ptr<TrainHandler> TrainHandlerPtr;
+  virtual void setWeight(const float &w) = 0;
+};
+typedef std::unique_ptr<TrainHandler> TrainHandlerPtr;
 
-    /**
-     * @brief An object storing tunable factors, whose weights can be tuned with training
-     */
-    class Trainable 
-        : virtual public strct::NodesAware
-        , virtual public strct::BeliefAware
-        , virtual public strct::StructureTunableAware
+/**
+ * @brief An object storing tunable factors, whose weights can be tuned with
+ * training
+ */
+class Trainable : virtual public strct::NodesAware,
+                  virtual public strct::BeliefAware,
+                  virtual public strct::StructureTunableAware
 #ifdef THREAD_POOL_ENABLED
-        , virtual public strct::ThreadPoolAware
+    ,
+                  virtual public strct::ThreadPoolAware
 #endif
-    {
-    public:
-        /**
-         * @param the new set of weights to assume for the tunable clusters
-         * @throw when the number of passed weights is inconsistent
-         */
-        void setWeights(const std::vector<float>& w);
+{
+public:
+  /**
+   * @param the new set of weights to assume for the tunable clusters
+   * @throw when the number of passed weights is inconsistent
+   */
+  void setWeights(const std::vector<float> &w);
 
-        /**
-         * @param sets equal to 1 the weight of all the tunable clusters
-         */
-        void setOnes();
+  /**
+   * @param sets equal to 1 the weight of all the tunable clusters
+   */
+  void setOnes();
 
-        /**
-         * @return the gradient of the weights of the tunable clusters w.r.t. the passed training set
-         * @throw when the trainSet is inconsistent because have a wrong combinations size
-         */
-        virtual std::vector<float> getGradient(TrainSetPtr trainSet) = 0;
+  /**
+   * @return the gradient of the weights of the tunable clusters w.r.t. the
+   * passed training set
+   * @throw when the trainSet is inconsistent because have a wrong combinations
+   * size
+   */
+  virtual std::vector<float> getGradient(TrainSetPtr trainSet) = 0;
 
-    protected:
-        virtual TrainHandlerPtr makeHandler(std::shared_ptr<distribution::factor::modif::FactorExponential> factor);
-        void insertHandler(std::shared_ptr<distribution::factor::modif::FactorExponential> factor);
+  void train(::train::Trainer &solver, TrainSetPtr trainSet);
+  void train(::train::Trainer &solver, TrainSetPtr trainSet,
+             const float percentage);
 
-        std::list<TrainHandlerPtr> handlers;
+protected:
+  virtual TrainHandlerPtr makeHandler(
+      std::shared_ptr<distribution::factor::modif::FactorExponential> factor);
+  void insertHandler(
+      std::shared_ptr<distribution::factor::modif::FactorExponential> factor);
 
-        void setTrainSet(TrainSetPtr newSet);
-        inline TrainSetPtr getLastTrainSet() const { return this->lastTrainSet; }
+  std::list<TrainHandlerPtr> handlers;
 
-    private:
-        TrainSetPtr lastTrainSet;
-    };
-}
+  void Trainable::setTrainSet(TrainSetPtr newSet);
+  class WeightsAware;
+
+private:
+  TrainSetPtr lastTrainSet;
+};
+} // namespace EFG::train
 
 #endif
