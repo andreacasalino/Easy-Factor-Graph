75075db46dc6a8724e1251cc900b5408846a8afc
AndreaCasalino <andrea.casalino@cloudnc.com>
2022-05-02 16:19:18 +0100

    testing

---
+++ tests/launch.sh
@@ -0,0 +1,26 @@
+./Tests "[range]"
+
+./Tests "[distribution]"
+
+./Tests "[factor]"
+
+./Tests "[factor]"
+
+./Tests "[factor-special]"
+
+./Tests "[insertion]"
+
+./Tests "[evidence]"
+
+./Tests "[io]"
+
+./Tests "[propagation]"
+
+./Tests "[pool]"
+
+./Tests "[gibbs_sampling]"
+
+./Tests "[gradient]"
+
+# ./Tests "[train]"
+
