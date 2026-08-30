bacdf1b056bbbebe0f436ced4fe20360d179f9b6
Foo <foo:gmail.com>
2026-08-30 22:23:10 +0200

    impl

--- tests/Test00.cpp
+++ tests/Test00.cpp
@@ -2,6 +2,7 @@
 #include <catch2/generators/catch_generators.hpp>
 
 #include <EasyFactorGraph/misc/Generator.h>
+#include <EasyFactorGraph/misc/Samples.h>
 #include <EasyFactorGraph/misc/WorkerPool.h>
 
 #include <Utils.h>
@@ -20,6 +21,34 @@ TEST_CASE("generator materialization", "[misc]") {
     values = {7, 4, 6, 8, 8, 2};
 
     auto values_back = materialize<int>(GenFromBlock<int>{values});
+    CHECK(values == values_back);
+  }
+}
+
+TEST_CASE("Samples iterator", "[misc]") {
+  Samples samples{5};
+
+  SECTION("Add invalid sample") { CHECK_THROWS(samples.add({1, 1})); }
+
+  SECTION("add thn iterate") {
+    std::vector<std::vector<categoric::VarStateSize>> values;
+    for (std::size_t k = 0; k < 10; ++k) {
+      auto &added = values.emplace_back();
+      added.resize(5, k);
+      samples.add(added);
+    }
+
+    std::vector<std::vector<categoric::VarStateSize>> values_back;
+    auto it = samples.makeIter();
+    while (true) {
+      auto maybe_next = it.next();
+      if (!maybe_next) {
+        break;
+      }
+      values_back.emplace_back(std::vector<categoric::VarStateSize>{
+          maybe_next->begin(), maybe_next->end()});
+    }
+
     CHECK(values == values_back);
   }
 }
